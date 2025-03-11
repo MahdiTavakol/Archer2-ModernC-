7cadc6eaffdc518254a85fb70bdce5fc3a65ec1f
Mahdi Tavakol <mahditavakol90@gmail.com>
2025-03-11 21:44:43 +0000

    Solved part 1 of the container exercise

--- exercises/containers/part2/vector_ex.cpp
+++ exercises/containers/part2/vector_ex.cpp
@@ -4,5 +4,25 @@
 // https://en.cppreference.com/w/cpp/container/vector
 
 std::vector<int> GetEven(std::vector<int> const& source) {
+    std::vector<int> evens;
+
+    for (const int& elem: source)
+        if (elem % 2 == 0)
+            evens.push_back(elem);
+
+    return evens;
 }
 
+void PrintVectorOfInt(std::ostream& output, std::vector<int> const& data)
+{
+    output << "[ ";
+    bool first = true;
+    for (auto const& x: data) {
+        if (!first) {
+            output << ", ";
+        }
+        first = false;
+        output << x;
+    }
+    output << "]";
+}
