09e98a66c4474947918314dfcf34a7441e8f3db7
Mahdi Tavakol <mahditavakol90@gmail.com>
2025-03-11 17:04:25 +0000

    Solved the complex exercise

--- exercises/complex/complex.cpp
+++ exercises/complex/complex.cpp
@@ -1,18 +1,20 @@
 #include "complex.hpp"
 #include <cmath>
 
+Complex::Complex(const double& _re): re(_re), im(0.0) {}
 
+Complex::Complex(const double& _re, const double& _im): re(_re), im(_im) {}
 
-double const& Complex::real() {
+const double& Complex::real() const {
   return re;
 }
 
-double Complex::imag() const {
+const double& Complex::imag() const {
   return im;
 }
 
 Complex Complex::conj() const {
-  return Complex{re, -im};
+  return Complex(re, -im);
 }
 
 double Complex::norm() const {
@@ -24,21 +26,42 @@ double Complex::norm2() const {
 }
 
 bool operator==(Complex const& a, Complex const& b) {
-  return (a.re == b.re) && (a.im == b.re);
+  return (a.re == b.re) && (a.im == b.im);
 }
 bool operator!=(Complex const& a, Complex const& b) {
   return !(a == b);
 }
 
 Complex operator+(Complex const& a, Complex const& b) {
-  return Complex{a.re + b.re, a.im + b.im};
+  return Complex(a.re + b.re, a.im + b.im);
 }
 
+Complex operator-(Complex const& a, Complex const& b) {
+  return Complex(a.re - b.re, a.im - b.im);
+}
 
 Complex operator*(Complex const& a, Complex const& b) {
+  return Complex(a.re*b.re-a.im*b.im, a.re*b.im+a.im*b.re);
   // (a + ib)*(c + id) == (a*c - b*d) + i(b*c + a*d)
 }
 
+Complex operator/(Complex const& a, Complex const& b) {
+
+  double denominator = b.re * b.re + b.im * b.im;
+  return Complex((a.re * b.re + a.im * b.im) / denominator,
+                 (a.im * b.re - a.re * b.im) / denominator);
+}
+
+Complex operator*(Complex const& a, int const& b)
+{
+  return Complex(b*a.re,b*a.im); 
+}
+
+Complex operator/(Complex const& a, int const& b)
+{
+  return Complex(a.re/b,a.im/b);
+}
+
 Complex operator-(Complex const& a) {
-  return Complex{-a.re, -a.im};
+  return Complex(-a.re, -a.im);
 }
--- exercises/complex/complex.hpp
+++ exercises/complex/complex.hpp
@@ -7,14 +7,16 @@ public:
   // Default value is zero
   Complex() = default;
   // Construct purely real complex
+  Complex(const double& _re);
   // Construct from real and imaginary parts
+  Complex(const double& _re, const double& _im);
 
   // Access components
-  double real() const;
-  double imag() const;
+  const double& real() const;
+  const double& imag() const;
 
   // Compute the complex conjugate
-  Complex conj();
+  Complex conj() const;
 
   // Compute the magnitude and squared magnitude
   double norm() const;
@@ -30,6 +32,8 @@ public:
   friend Complex operator*(Complex const& a, Complex const& b);
   friend Complex operator/(Complex const& a, Complex const& b);
   // Question: how would you declare multiplication and division by a real number?
+  friend Complex operator*(Complex const& a, int const& b);
+  friend Complex operator/(Complex const& a, int const& b);
 
   // Unary negation
   friend Complex operator-(Complex const& a);
